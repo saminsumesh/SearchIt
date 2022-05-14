445402e6363ad213c7f6d033abafbf604fc61586
Zac <91833744+saminsumesh@users.noreply.github.com>
2022-05-14 11:43:11 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -3,7 +3,7 @@ from urllib.parse import quote
 
 from pyrogram import Client, emoji, filters
 from pyrogram.errors import UserNotParticipant
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle
 
 from utils import get_search_results, InlineQueryResultCachedDocument
 from info import CACHE_TIME, SHARE_BUTTON_TEXT, AUTH_USERS, AUTH_CHANNEL
